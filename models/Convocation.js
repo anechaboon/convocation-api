bf6f35c4a7f779064500f84e4bc855d48a7b3fc2
anecha.boon <102786066+anecha-petch@users.noreply.github.com>
2024-08-25 13:51:01 +0700

    revise logic data row seat convocation

--- models/Convocation.js
+++ models/Convocation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const convocationSchema = new mongoose.Schema({
   endColumn: { type: Number, required: true },
-  endRow: { type: String, required: true },
+  endRow: { type: Number, required: true },
   // date: { type: String, required: true },
   reserved: { type: String },
   allSeat: { type: Number, required: true, default: 1 },
